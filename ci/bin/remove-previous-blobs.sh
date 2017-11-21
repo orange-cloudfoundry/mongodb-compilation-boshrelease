98f8ab428dfac6675bb51acf81bc285e242e7f65
Jérémy Raverdy <jeremy.raverdy@orange.com>
2017-11-21 14:33:24 +0100

    Adding task to remove previous versions from blobs.yml

---
+++ ci/bin/remove-previous-blobs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash 
+
+set -exc
+
+ROOT_FOLDER=${PWD}
+
+pushd to-upload
+
+for i in $(find ${ROOT_FOLDER}/downloaded-src -type f -name '*.tar.gz' -print)
+do
+  product=$(basename $(dirname $i))
+  archive=$(basename $i)
+  version=$(${ROOT_FOLDER}/mongodb-compilation-bosh-release/ci/bin/get-archive-version.pl ${archive})
+  prefix=$(echo ${archive}|sed -e 's/\(.*\)$version.*/\1/')
+  bosh -e $ALIAS blobs| grep "^${product}/${prefix}" | grep -v $version | cut -d" " -f1
+done
+
+popd
