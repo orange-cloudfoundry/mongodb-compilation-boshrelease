e5aab6c18bd60553b60d07dfe51f25b933cfdbe4
Jérémy Raverdy <jeremy.raverdy@orange.com>
2017-11-21 16:10:45 +0100

    perform bosh remove-blob

--- ci/bin/remove-previous-blobs.sh
+++ ci/bin/remove-previous-blobs.sh
@@ -12,7 +12,12 @@ do
   archive=$(basename $i)
   version=$(${ROOT_FOLDER}/mongodb-compilation-bosh-release/ci/bin/get-archive-version.pl ${archive})
   prefix=$(echo ${archive}|sed -e "s/\(.*\)${version}.*/\1/")
-  bosh -e $ALIAS blobs| grep "^${product}/${prefix}" | grep -v $version | cut -d" " -f1
+
+  # removing old blobs
+  for j in $(bosh -e $ALIAS blobs| grep "^${product}/${prefix}" | grep -v $version | cut -d" " -f1)
+  do
+  	bosh -e $ALIAS -n remove-blobs $j
+  done
 done
 
 popd
