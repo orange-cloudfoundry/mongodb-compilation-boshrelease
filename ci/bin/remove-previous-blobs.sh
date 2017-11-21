81c60205192266370eaff38c18c855e27061c586
Jérémy Raverdy <jeremy.raverdy@orange.com>
2017-11-21 16:03:54 +0100

    wrong quote type in sed

--- ci/bin/remove-previous-blobs.sh
+++ ci/bin/remove-previous-blobs.sh
@@ -11,7 +11,7 @@ do
   product=$(basename $(dirname $i))
   archive=$(basename $i)
   version=$(${ROOT_FOLDER}/mongodb-compilation-bosh-release/ci/bin/get-archive-version.pl ${archive})
-  prefix=$(echo ${archive}|sed -e 's/\(.*\)$version.*/\1/')
+  prefix=$(echo ${archive}|sed -e "s/\(.*\)${version}.*/\1/")
   bosh -e $ALIAS blobs| grep "^${product}/${prefix}" | grep -v $version | cut -d" " -f1
 done
 
