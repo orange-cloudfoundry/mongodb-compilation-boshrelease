af761e464e4c536483514111790568a4de33716b
Jérémy Raverdy <jeremy.raverdy@orange.com>
2017-11-22 10:10:22 +0100

    create a function for fake files

--- ci/bin/create-release.sh
+++ ci/bin/create-release.sh
@@ -2,42 +2,49 @@
 
 set -ex
 
+create_fake_files()
+{
+    # Creating fake files for already deployed releases
+
+    if [ ! -d dev_releases/${BOSH_RELEASE} ]
+    then
+        mkdir -p dev_releases/${BOSH_RELEASE}
+    fi
+
+    for release in $(bosh -e ${ALIAS} releases -d ${DEPLOYMENT_NAME} --column="Version")
+    do
+        # remove the * at the end of deployed releases
+        release=$(echo $release|sed -e "s/\*$//")
+
+        # get the hash of the release
+        commit_hash=$(bosh -e ${ALIAS} releases -d ${DEPLOYMENT_NAME} --column="Version" --column="commit hash" \
+                    |grep -w "^${release}"|tr -s [:space:]|cut -d" " -f2)
+
+        if [ ! -f dev_releases/${BOSH_RELEASE}/index.yml ]
+        then
+            echo "builds:" > dev_releases/${BOSH_RELEASE}/index.yml
+        fi
+        if [ ! -f dev_releases/${BOSH_RELEASE}/${BOSH_RELEASE}-${release}.yml ]
+        then
+            cat > dev_releases/${BOSH_RELEASE}/${BOSH_RELEASE}-${release}.yml <<EOF
+            name: ${DEPLOYMENT_NAME}
+            version: ${release}
+            commit_hash: ${commit_hash}
+            uncommitted_changes: false
+EOF
+            cat >> dev_releases/${BOSH_RELEASE}/index.yml <<EOF
+            $(cat /proc/sys/kernel/random/uuid):
+            version: ${release}
+EOF
+        fi
+    done    
+}
+
 export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
 
 pushd mongodb-compilation-bosh-release|| exit 666
 
-# Creating fake files for already deployed releases
-if [ ! -d dev_releases/${BOSH_RELEASE} ]
-then
-    mkdir -p dev_releases/${BOSH_RELEASE}
-fi
-
-for release in $(bosh -e ${ALIAS} releases -d ${DEPLOYMENT_NAME} --column="Version")
-do
-    # remove the * at the end of deployed releases
-    release=$(echo $release|sed -e "s/\*$//")
-
-    # get the hash of the release
-    commit_hash=$(bosh -e ${ALIAS} releases -d ${DEPLOYMENT_NAME} --column="Version" --column="commit hash" \
-                |grep  "^${release}"|tr -s [:space:]|cut -d" " -f2)
-    if [ ! -f dev_releases/${BOSH_RELEASE}/index.yml ]
-    then
-        echo "builds:" > dev_releases/${BOSH_RELEASE}/index.yml
-    fi
-    if [ ! -f dev_releases/${BOSH_RELEASE}/${BOSH_RELEASE}-${release}.yml ]
-    then
-        cat > dev_releases/${BOSH_RELEASE}/${BOSH_RELEASE}-${release}.yml <<EOF
-        name: ${DEPLOYMENT_NAME}
-        version: ${release}
-        commit_hash: ${commit_hash}
-        uncommitted_changes: false
-EOF
-        cat >> dev_releases/${BOSH_RELEASE}/index.yml <<EOF
-        $(cat /proc/sys/kernel/random/uuid):
-        version: ${release}
-EOF
-    fi
-done
+create_fake_files
 
 bosh -e ${ALIAS} create-release --force
 
