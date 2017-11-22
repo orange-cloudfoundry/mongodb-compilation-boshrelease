9c9043b1d9ca2d69cad7af5814960c78979484ea
Jérémy Raverdy <jeremy.raverdy@orange.com>
2017-11-22 02:12:35 +0100

    better resource definition

--- ci/bin/upload-blob-list.sh
+++ ci/bin/upload-blob-list.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+ROOT_FOLDER=${PWD}
+
 mkdir -p ~/.aws
 
 # create cert file needed for aws
@@ -15,5 +17,7 @@ cd to-upload || exit 666
 #upload blob list
 aws --endpoint-url $ENDPOINT_URL --no-verify-ssl s3 cp config/blobs.yml s3://$BUCKET/ci/blobs.yml
 
+mkdir -p ${ROOT_FOLDER}/uploaded
+cd ${ROOT_FOLDER}/uploaded || exit 666
 # create keyval file to indicate the end of sources upload job
-echo "date=$(date +%Y-%d-%m %H24:M%)" > update-ok.properties
+echo "upload_date=\"$(date '+%Y-%d-%m %H:%M')\"" > keyval.properties
