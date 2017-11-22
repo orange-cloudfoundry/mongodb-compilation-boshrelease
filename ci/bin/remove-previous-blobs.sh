037c2ab5351a5d67cbd1c23e42d5059202135a27
Jérémy Raverdy <jeremy.raverdy@orange.com>
2017-11-22 11:38:56 +0100

    Bad blob checking because of tab characters

--- ci/bin/remove-previous-blobs.sh
+++ ci/bin/remove-previous-blobs.sh
@@ -10,16 +10,17 @@ pushd to-upload
 
 for i in $(find ${ROOT_FOLDER}/downloaded-src -type f -name '*.tar.gz' -print)
 do
-  product=$(basename $(dirname $i))
-  archive=$(basename $i)
-  version=$(${ROOT_FOLDER}/mongodb-compilation-bosh-release/ci/bin/get-archive-version.pl -v ${archive})
-  prefix=$(${ROOT_FOLDER}/mongodb-compilation-bosh-release/ci/bin/get-archive-version.pl -p ${archive})
+    product="$(basename $(dirname $i))"
+    archive="$(basename $i)"
+    version=$(${ROOT_FOLDER}/mongodb-compilation-bosh-release/ci/bin/get-archive-version.pl -v ${archive})
+    prefix=$(${ROOT_FOLDER}/mongodb-compilation-bosh-release/ci/bin/get-archive-version.pl -p ${archive})
 
-  # removing old blobs
-  for j in $(bosh -e $ALIAS blobs| grep "^${product}/${prefix}" | grep -v $version | cut -d" " -f1)
-  do
-  	bosh -e $ALIAS -n remove-blob $j
-  done
+    # removing old blobs
+    for j in $(bosh -e $ALIAS blobs|sed -e $"s/\t/ /g" | cut -d" " -f1 | grep "^${product}/${prefix}" \
+                                    | grep -v "$version" | tr -s [:space:] )
+    do
+        bosh -e $ALIAS -n remove-blob $j
+    done
 
 done
 
