a1ed460ab481b299ebb242b1999c3eb7483fb67f
Jérémy Raverdy <jeremy.raverdy@orange.com>
2017-11-24 14:45:21 +0100

    No need to get src to perform errand

--- ci/bin/make-archive.sh
+++ ci/bin/make-archive.sh
@@ -3,22 +3,16 @@
 set -ex
 
 export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
-pushd mongodb-compilation-bosh-release || exit 666
 
 bosh -e ${ALIAS} -d ${DEPLOYMENT_NAME} -n run-errand make-tar --keep-alive
 
 src_vm=$(bosh -e ${ALIAS} -d ${DEPLOYMENT_NAME} vms --column="instance"|tr -d [:space:])
 
 bosh -e ${ALIAS} -d ${DEPLOYMENT_NAME} scp \
-${src_vm}:/var/vcap/store/make-tar/archive/mongodb-linux-x86_64-*.tar.gz .
+${src_vm}:/var/vcap/store/make-tar/archive/mongodb-linux-x86_64-*.tar.gz ${ROOT_FOLDER}/to-upload
 
 file=`ls mongodb-linux-x86_64-*.tar.gz`
-echo file: $file
 bosh -e ${ALIAS} add-blob $file mongodb/$file
 bosh -e ${ALIAS} upload-blobs
 bosh -e ${ALIAS} -d ${DEPLOYMENT_NAME} -n delete-vm $(bosh -e ${ALIAS} -d \
   ${DEPLOYMENT_NAME} vms --column="vm cid")
-
-popd
-
-rsync -ra mongodb-compilation-bosh-release/ to-upload/
