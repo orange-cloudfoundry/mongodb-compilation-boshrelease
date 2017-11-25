0c3a8a705330402edbfc83db701091782fc39f0e
Jérémy Raverdy <jeremy.raverdy@orange.com>
2017-11-25 02:35:18 +0100

    not in the right directory

--- ci/bin/make-archive.sh
+++ ci/bin/make-archive.sh
@@ -13,7 +13,10 @@ src_vm=$(bosh -e ${ALIAS} -d ${DEPLOYMENT_NAME} vms --column="instance"|tr -d [:
 bosh -e ${ALIAS} -d ${DEPLOYMENT_NAME} scp \
 ${src_vm}:/var/vcap/store/make-tar/archive/mongodb-linux-x86_64-*.tar.gz ${ROOT_FOLDER}/to-upload
 
+pushd to-upload || exit 666
+
 file=`ls mongodb-linux-x86_64-*.tar.gz`
+
 bosh -e ${ALIAS} add-blob $file mongodb/$file
 bosh -e ${ALIAS} upload-blobs
 bosh -e ${ALIAS} -d ${DEPLOYMENT_NAME} -n delete-vm $(bosh -e ${ALIAS} -d \
