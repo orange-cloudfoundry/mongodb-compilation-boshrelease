0c0d7f449372aac41da5be243baad95231bb7a76
Jérémy Raverdy <jeremy.raverdy@orange.com>
2017-11-27 11:00:09 +0100

    correcting errors on never initialized CI (not available files on s3)

--- ci/bin/get-config-files.sh
+++ ci/bin/get-config-files.sh
@@ -20,10 +20,12 @@ cp -rp ${ROOT_FOLDER}/mongodb-compilation-bosh-release/. ${ROOT_FOLDER}/mongodb-
 cd mongodb-compilation-bosh-release-patched || exit 666
 
 #retrieve blob list
-aws --endpoint-url ${ENDPOINT_URL} --no-verify-ssl s3 cp s3://${BUCKET}/ci/blobs.yml config/blobs.yml
+aws --endpoint-url ${ENDPOINT_URL} --no-verify-ssl s3 cp s3://${BUCKET}/ci/blobs.yml config/blobs.yml \
+||echo "no archived blobs.yml, use release default one"
 
 #retrieve final.yml
-aws --endpoint-url ${ENDPOINT_URL} --no-verify-ssl s3 cp s3://${BUCKET}/ci/final.yml config/final.yml
+aws --endpoint-url ${ENDPOINT_URL} --no-verify-ssl s3 cp s3://${BUCKET}/ci/final.yml config/final.yml \
+||echo "no archived final.yml, use release default one"
 
 #get the list of availables blobs ids on blobsore
 aws --endpoint-url ${ENDPOINT_URL} --no-verify-ssl s3 ls s3://${BUCKET}/ > blobstore_ids.list
--- ci/bin/upload-bosh.sh
+++ ci/bin/upload-bosh.sh
@@ -13,15 +13,19 @@ for i in $(find ${ROOT_FOLDER}/downloaded-src -type f -name '*.tar.gz' -print)
 do
   product=$(basename $(dirname $i))
   archive=$(basename $i)
+  
   # do not upload again already available blob - prevent blobstore to have the same blob twice
-  available=$(grep $(bosh blobs --column="path" --column="blobstore id" \
-                |grep "^${product}/${archive}" |tr -s "\t" " "|tr -s [:space:]|cut -d" " -f2) blobstore_ids.list|wc -l)
-  if [ $available -eq 0 ]
+  blobstore_id=$(bosh blobs --column="path" --column="blobstore id" \
+                |grep "^${product}/${archive}" |tr -s "\t" " "|tr -s [:space:]|cut -d" " -f2)
+  [ "${blostore_id}" != "" ] && available=$(grep ${blostore_id} blobstore_ids.list|wc -l) || available=0
+
+  if [ ${available} -eq 0 ]
   then
     bosh add-blob $i ${product}/${archive}
   else
     echo "blob ^${product}/${archive} is already present in blobstore"   
   fi
+
 done
 bosh -e ${ALIAS} upload-blobs
 
