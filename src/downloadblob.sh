ae1c5ba55be4d06ec5e0dd528080dbe1c274892a
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-01-31 15:16:25 +0100

    Change curl policies for sources download

--- src/downloadblob.sh
+++ src/downloadblob.sh
@@ -1,22 +1,22 @@
-curl --retry 5 -o cmake-3.8.2.tar.gz -L https://cmake.org/files/v3.8/cmake-3.8.2.tar.gz
-curl --retry 5 -o bzip2-1.0.6.tar.gz -L http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz
-curl --retry 5 -o lz4-1.7.5.tar.gz -L https://github.com/lz4/lz4/archive/v1.7.5.tar.gz
-curl --retry 5 -o snappy-1.1.4.tar.gz -L https://github.com/google/snappy/archive/1.1.5.tar.gz
-curl --retry 5 -o zlib-1.2.11.tar.gz -L https://zlib.net/zlib-1.2.11.tar.gz
-curl --retry 5 -o zstd-1.3.0.tar.gz -L https://github.com/facebook/zstd/archive/v1.3.0.tar.gz
-curl --retry 5 -o gcc-5.4.0.tar.gz -L https://ftp.gnu.org/gnu/gcc/gcc-5.4.0/gcc-5.4.0.tar.gz
-curl --retry 5 -o gmp-6.1.2.tar.bz2 -L https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2
-curl --retry 5 -o libpcap-1.8.1.tar.gz -L http://www.tcpdump.org/release/libpcap-1.8.1.tar.gz
-curl --retry 5 -o m4-1.4.18.tar.bz2 -L https://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.bz2
-curl --retry 5 -o mpc-1.0.3.tar.gz -L https://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz
-curl --retry 5 -o mpfr-3.1.5.tar.bz2 -L https://ftp.gnu.org/gnu/mpfr/mpfr-3.1.5.tar.bz2
-curl --retry 5 -o isl-0.18.tar.gz -L http://isl.gforge.inria.fr/isl-0.18.tar.gz
-curl --retry 5 -o Python-2.7.13.tgz -L https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz
-curl --retry 5 -o scons-2.5.1.tar.gz -L https://ftp.osuosl.org/pub/blfs/conglomeration/scons/scons-2.5.1.tar.gz
-curl --retry 5 -o openssl-1.0.2l.tar.gz -L https://www.openssl.org/source/openssl-1.0.2l.tar.gz
-curl --retry 5 -o lzip-1.19.tar.gz -L http://download.savannah.gnu.org/releases/lzip/lzip-1.19.tar.gz
-curl --retry 5 -o ed-1.14.2.tar.lz -L https://ftp.gnu.org/gnu/ed/ed-1.14.2.tar.lz
-curl --retry 5 -o texinfo-6.4.tar.gz -L https://ftp.gnu.org/gnu/texinfo/texinfo-6.4.tar.gz
-curl --retry 5 -o bc-1.07.1.tar.gz -L https://ftp.gnu.org/gnu/bc/bc-1.07.1.tar.gz
-curl --retry 5 -o binutils-2.28.tar.gz -L https://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz
-curl --retry 5 -o coreutils-8.27.tar.xz -L https://ftp.gnu.org/gnu/coreutils/coreutils-8.27.tar.xz
+curl --connect-timeout 10 -w 200 --retry 5 -o cmake-3.8.2.tar.gz -L https://cmake.org/files/v3.8/cmake-3.8.2.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o bzip2-1.0.6.tar.gz -L http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o lz4-1.7.5.tar.gz -L https://github.com/lz4/lz4/archive/v1.7.5.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o snappy-1.1.4.tar.gz -L https://github.com/google/snappy/archive/1.1.5.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o zlib-1.2.11.tar.gz -L https://zlib.net/zlib-1.2.11.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o zstd-1.3.0.tar.gz -L https://github.com/facebook/zstd/archive/v1.3.0.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o gcc-5.4.0.tar.gz -L https://ftp.gnu.org/gnu/gcc/gcc-5.4.0/gcc-5.4.0.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o gmp-6.1.2.tar.bz2 -L https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2
+curl --connect-timeout 10 -w 200 --retry 5 -o libpcap-1.8.1.tar.gz -L http://www.tcpdump.org/release/libpcap-1.8.1.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o m4-1.4.18.tar.bz2 -L https://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.bz2
+curl --connect-timeout 10 -w 200 --retry 5 -o mpc-1.0.3.tar.gz -L https://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o mpfr-3.1.5.tar.bz2 -L https://ftp.gnu.org/gnu/mpfr/mpfr-3.1.5.tar.bz2
+curl --connect-timeout 10 -w 200 --retry 5 -o isl-0.18.tar.gz -L http://isl.gforge.inria.fr/isl-0.18.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o Python-2.7.13.tgz -L https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz
+curl --connect-timeout 10 -w 200 --retry 5 -o scons-2.5.1.tar.gz -L https://ftp.osuosl.org/pub/blfs/conglomeration/scons/scons-2.5.1.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o openssl-1.0.2l.tar.gz -L https://www.openssl.org/source/openssl-1.0.2l.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o lzip-1.19.tar.gz -L http://download.savannah.gnu.org/releases/lzip/lzip-1.19.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o ed-1.14.2.tar.lz -L https://ftp.gnu.org/gnu/ed/ed-1.14.2.tar.lz
+curl --connect-timeout 10 -w 200 --retry 5 -o texinfo-6.4.tar.gz -L https://ftp.gnu.org/gnu/texinfo/texinfo-6.4.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o bc-1.07.1.tar.gz -L https://ftp.gnu.org/gnu/bc/bc-1.07.1.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o binutils-2.28.tar.gz -L https://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o coreutils-8.27.tar.xz -L https://ftp.gnu.org/gnu/coreutils/coreutils-8.27.tar.xz
