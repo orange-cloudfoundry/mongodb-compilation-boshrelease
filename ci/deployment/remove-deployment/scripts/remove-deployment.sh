61ec4053df733da9d5421eab901cfc8064223116
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-02-06 15:25:14 +0100

    Removing release too

--- ci/deployment/remove-deployment/scripts/remove-deployment.sh
+++ ci/deployment/remove-deployment/scripts/remove-deployment.sh
@@ -6,8 +6,11 @@ export ROOT_FOLDER=${PWD}
 
 export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
 
-bosh -e ${ALIAS} -n -d mongodb-ci-rs \
+bosh -e ${ALIAS} -n -d ${DEPLOYMENT_NAME} \
 		delete-deployment
+
+# remove release
+bosh -e ${ALIAS} -n delete-release ${RELEASE_NAME}
 
 # removing orphaned disks
 
@@ -21,5 +24,5 @@ done
 mkdir -p removed
 
 pushd removed || exit 666
-echo "Deployment_deletion_date=$(date '+%Y-%d-%m %H:%M')"> keyval.properties
+touch keyval.properties
 popd
