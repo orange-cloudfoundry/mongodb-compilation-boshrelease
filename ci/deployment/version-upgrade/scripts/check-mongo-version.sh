7b0f3ba004aeaa8592d9e2e82ac3dc8bdb23d7c0
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-02-06 13:57:18 +0100

    Check version after upgrade

---
+++ ci/deployment/version-upgrade/scripts/check-mongo-version.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+set -ex
+
+export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
+
+ROOT_FOLDER=${PWD}
+
+cat ${ROOT_FOLDER}/deployment-specs/keyval.properties \
+	| grep -v -E "^UPDATED|^UUID" \
+	> ${ROOT_FOLDER}/deployment-specs/sourced.properties
+
+source ${ROOT_FOLDER}/deployment-specs/sourced.properties 
+
+CI_IP=`echo ${ips} \
+	| sed -e "s/,/:${PORT},/g" -e "s/$/:${PORT}/"`
+
+# get mongodb server version
+
+installed_version=$(mongo --host rs0/${CI_IP} -u ${USER} -p "${password}" --authenticationDatabase admin \
+  --eval "db.version()"|tail -1)
+
+needed_version=$(grep "^mongodb" ${ROOT_FOLDER}/versions/keyval.properties|cut -d"=" -f2)
+
+if [ "${installed_version}" != "${needed_version}" ] 
+then
+	echo "Mongodb server version is ${installed_version} and don\'t match expected one (${needed_version})" \
+  exit 666
+fi
