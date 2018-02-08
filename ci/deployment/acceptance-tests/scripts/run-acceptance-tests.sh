681e4ca0ea9c078974dad3edf2e67b6ede67763e
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-02-08 14:20:47 +0100

    suffix release and deployment name on centos deployments

--- ci/deployment/acceptance-tests/scripts/run-acceptance-tests.sh
+++ ci/deployment/acceptance-tests/scripts/run-acceptance-tests.sh
@@ -6,4 +6,11 @@ export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
 
 ROOT_FOLDER=${PWD}
 
+if [ "${STEMCELL_TYPE}" == "centos" ]
+then
+    # If we are on a centos deployment, deloyment name and release name will be suffixed
+    DEPLOYMENT_NAME="${DEPLOYMENT_NAME}-centos"
+    RELEASE_NAME="${RELEASE_NAME}-centos"
+fi
+
 bosh -e ${ALIAS} -d mongodb-ci-rs run-errand acceptance-tests
