b0b140e9b4c62dd753be7ef77b4f1848e3aa018d
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-03-01 15:00:45 +0100

    Rewriting create_fake_files function

--- ci/deployment/deploy/scripts/deploy-rs.sh
+++ ci/deployment/deploy/scripts/deploy-rs.sh
@@ -13,33 +13,38 @@ create_fake_files()
         mkdir -p dev_releases/${RELEASE_NAME}
     fi
 
-    for release in $(bosh -e ${ALIAS} releases | grep ${RELEASE_NAME} \
-                    | sed -e 's/[^[:space:]]*[[:space:]]*\([^[:space:]]*\).*/\1/' | tr -d "*")
-    do
-
-        # get the hash of the release
-        commit_hash=$(bosh -e ${ALIAS} releases --column="Version" --column="commit hash" \
-                    | tr -d "*" | grep -w "^${release}" | tr -s "\t" " "|cut -d" " -f2 \
-                    | tr -d [:space:] | tr -d "+")
-
-        if [ ! -f dev_releases/${RELEASE_NAME}/index.yml ]
-        then
-            echo "builds:" > dev_releases/${RELEASE_NAME}/index.yml
-        fi
-        if [ ! -f dev_releases/${RELEASE_NAME}/${RELEASE_NAME}-${release}.yml ]
-        then
-            cat > dev_releases/${RELEASE_NAME}/${RELEASE_NAME}-${release}.yml <<EOF
-name: ${DEPLOYMENT_NAME}
-version: ${release}
-commit_hash: ${commit_hash}
-uncommitted_changes: false
-EOF
-            cat >> dev_releases/${RELEASE_NAME}/index.yml <<EOF
-  $(cat /proc/sys/kernel/random/uuid):
-    version: ${release}
-EOF
-        fi
-    done    
+    bosh -e ${ALIAS} releases \
+            | cat \
+            | awk -v rn=${RELEASE_NAME} '{
+                                            if ($1==rn)
+                                                {
+                                                    gsub("\*","");
+                                                    printf "%s",$2;
+                                                    gsub("\+","");
+                                                    printf " %s\n",$3
+                                                }
+                                        }' \
+            | while read version commit_hash                            
+              do
+     
+                if [ ! -f dev_releases/${RELEASE_NAME}/index.yml ]
+                then
+                    echo "builds:" > dev_releases/${RELEASE_NAME}/index.yml
+                fi
+                if [ ! -f dev_releases/${RELEASE_NAME}/${RELEASE_NAME}-${version}.yml ]
+                then
+		            cat > dev_releases/${RELEASE_NAME}/${RELEASE_NAME}-${version}.yml <<-EOF
+							name: ${DEPLOYMENT_NAME}
+							version: ${version}
+							commit_hash: ${commit_hash}
+							uncommitted_changes: false
+							EOF
+		            cat >> dev_releases/${RELEASE_NAME}/index.yml <<-EOF
+							  $(cat /proc/sys/kernel/random/uuid):
+							    version: ${version}
+							EOF
+		        fi
+    		done    
 }
 
 export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
