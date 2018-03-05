6b7bc183f6f56ea879f966e7d5da2bfd9e30fd7c
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-03-05 15:08:14 +0100

    data check is done in specific task

--- ci/deployment/version-upgrade/scripts/check-mongo-version.sh
+++ ci/deployment/version-upgrade/scripts/check-mongo-version.sh
@@ -29,17 +29,6 @@ needed_version=$(grep "^mongodb" ${ROOT_FOLDER}/versions/keyval.properties|cut -
 
 if [ "${installed_version}" != "${needed_version}" ] 
 then
-	echo "Mongodb server version is ${installed_version} and don\'t match expected one (${needed_version})" \
+	echo "Mongodb server version is ${installed_version} and don\'t match expected one (${needed_version})"
   exit 666
 fi
-
-# checking values in db
-
-cat ${ROOT_FOLDER}/filled/keyval.properties| grep -v -E "^UPDATED|^UUID" |tr -s '=' ' '|while read x y
-do
-	mongo --host rs0/${CI_IP} -u ${USER} -p "${password}" --authenticationDatabase admin \
- 		--eval "if (db.testBackup.find({x:$x,y:$y}).count() == 0)
- 				{
- 					throw new Error('values (x:$x,y:$y) not found in collection');
- 				}" --quiet
-done
