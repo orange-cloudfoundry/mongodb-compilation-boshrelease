5c971b1e093ce1a185e9e1f13e67d0ffed5132ac
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-03-13 12:12:35 +0100

    Adding reinsert data job for test purposes

---
+++ ci/deployment/insertions-tests/scripts/reinsert-collection.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+set -ex
+
+export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
+
+ROOT_FOLDER=${PWD}
+
+cat ${ROOT_FOLDER}/deployment-specs/keyval.properties \
+  | grep -v -E "^UPDATED|^UUID" \
+  > ${ROOT_FOLDER}/deployment-specs/sourced.properties
+
+source ${ROOT_FOLDER}/deployment-specs/sourced.properties 
+
+if [ "${STEMCELL_TYPE}" != "centos" ]
+then
+  STEMCELL_TYPE="ubuntu"   
+fi
+
+CI_IP=`echo ${ips} \
+	 | sed -e "s/,/:${PORT},/g" -e "s/$/:${PORT}/"`
+
+# remove collection before insertion
+
+mongo --host rs0/${CI_IP} -u ${USER} -p "${password}" --authenticationDatabase admin \
+  --eval "db.testBackup.drop()"
+
+cat ${ROOT_FOLDER}/filled/keyval.properties| grep -v -E "^UPDATED|^UUID" |tr -s '=' ' '|while read x y
+do
+  mongo --host rs0/${CI_IP} -u ${USER} -p "${password}" --authenticationDatabase admin \
+    --eval "db.testBackup.insert( { x : ${x}, y : ${y} })" --quiet
+done
