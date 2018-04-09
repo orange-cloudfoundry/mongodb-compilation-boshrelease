af38189576218852adf83a28228fc478eb63265f
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-04-09 15:27:44 +0200

    Removing previous downlod scripts (now done by resource)

--- ci/compilation/upload-src/scripts/upload-bosh.sh
+++ ci/compilation/upload-src/scripts/upload-bosh.sh
@@ -9,7 +9,7 @@ rsync -ra mongodb-compilation-bosh-release-patched/ to-upload-pre/
 
 pushd to-upload-pre || exit 666
 
-for i in $(find ${ROOT_FOLDER}/downloaded-src -type f -name '*.tar.gz' -print)
+for i in $(find ${ROOT_FOLDER}/mongodb-src ${ROOT_FOLDER}/rocksdb-src -type f -name '*.tar.gz' -print)
 do
   product=$(basename $(dirname $i))
   archive=$(basename $i)
