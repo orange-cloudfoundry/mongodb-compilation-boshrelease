458504ee9bbef19f471ea61b7bae7389856f1ca4
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2018-12-06 11:51:57 +0100

    Use last release of ech item - trying gcc 8.2

--- src/downloadblob.sh
+++ src/downloadblob.sh
@@ -1,30 +1,53 @@
-curl --connect-timeout 10 -w 200 --retry 5 -o cmake-3.11.4.tar.gz -L https://github.com/Kitware/CMake/archive/v3.11.4.tar.gz
+
+# curl --connect-timeout 10 -w 200 --retry 5 -o isl-0.18.tar.gz -L http://isl.gforge.inria.fr/isl-0.18.tar.gz
+
+#binutils
+curl --connect-timeout 10 -w 200 --retry 5 -o binutils-2.31.1.tar.gz -L https://ftp.gnu.org/gnu/binutils/binutils-2.31.1.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o lzip-1.20.tar.gz -L http://download.savannah.gnu.org/releases/lzip/lzip-1.20.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o ed-1.14.2.tar.lz -L https://ftp.gnu.org/gnu/ed/ed-1.14.2.tar.lz
+curl --connect-timeout 10 -w 200 --retry 5 -o bc-1.07.1.tar.gz -L https://ftp.gnu.org/gnu/bc/bc-1.07.1.tar.gz
+
+#compilation tools
+curl --connect-timeout 10 -w 200 --retry 5 -o cmake-3.13.1.tar.gz -L https://github.com/Kitware/CMake/archive/v3.13.1.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o pkg-config-0.29.2.tar.gz -L https://pkg-config.freedesktop.org/releases/pkg-config-0.29.2.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o git-2.19.2.tar.gz -L https://github.com/git/git/archive/v2.19.2.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o expat-2.2.6.tar.gz -L https://github.com/libexpat/libexpat/archive/R_2_2_6.tar.gz
+
+#compressors
 curl --connect-timeout 10 -w 200 --retry 5 -o bzip2-1.0.6.tar.gz -L https://fossies.org/linux/misc/bzip2-1.0.6.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o lz4-1.8.2.tar.gz -L https://github.com/lz4/lz4/archive/v1.8.2.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o lz4-1.8.3.tar.gz -L https://github.com/lz4/lz4/archive/v1.8.3.tar.gz
 curl --connect-timeout 10 -w 200 --retry 5 -o snappy-1.1.7.tar.gz -L https://github.com/google/snappy/archive/1.1.7.tar.gz
 curl --connect-timeout 10 -w 200 --retry 5 -o zlib-1.2.11.tar.gz -L https://zlib.net/zlib-1.2.11.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o zstd-1.3.5.tar.gz -L https://github.com/facebook/zstd/archive/v1.3.5.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o gcc-5.4.0.tar.gz -L https://ftp.gnu.org/gnu/gcc/gcc-5.4.0/gcc-5.4.0.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o gmp-6.1.2.tar.bz2 -L https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2
-curl --connect-timeout 10 -w 200 --retry 5 -o libpcap-1.8.1.tar.gz -L http://www.tcpdump.org/release/libpcap-1.8.1.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o zstd-1.3.7.tar.gz -L https://github.com/facebook/zstd/archive/v1.3.7.tar.gz
+
+#coreutils
+curl --connect-timeout 10 -w 200 --retry 5 -o coreutils-8.30.tar.xz -L https://ftp.gnu.org/gnu/coreutils/coreutils-8.30.tar.xz
+
+#gcc
 curl --connect-timeout 10 -w 200 --retry 5 -o m4-1.4.18.tar.bz2 -L https://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.bz2
-curl --connect-timeout 10 -w 200 --retry 5 -o mpc-1.0.3.tar.gz -L https://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o mpfr-3.1.6.tar.bz2 -L https://ftp.gnu.org/gnu/mpfr/mpfr-3.1.6.tar.bz2
-curl --connect-timeout 10 -w 200 --retry 5 -o isl-0.18.tar.gz -L http://isl.gforge.inria.fr/isl-0.18.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o Python-2.7.15.tgz -L https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz
-curl --connect-timeout 10 -w 200 --retry 5 -o virtualenv-16.0.0.tar.gz -L https://github.com/pypa/virtualenv/archive/16.0.0.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o scons-3.0.1.tar.gz -L https://sourceforge.net/projects/scons/files/scons/3.0.1/scons-3.0.1.tar.gz/download
-curl --connect-timeout 10 -w 200 --retry 5 -o openssl-1.0.2o.tar.gz -L https://www.openssl.org/source/openssl-1.0.2o.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o lzip-1.19.tar.gz -L http://download.savannah.gnu.org/releases/lzip/lzip-1.19.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o ed-1.14.2.tar.lz -L https://ftp.gnu.org/gnu/ed/ed-1.14.2.tar.lz
-curl --connect-timeout 10 -w 200 --retry 5 -o texinfo-6.4.tar.gz -L https://ftp.gnu.org/gnu/texinfo/texinfo-6.4.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o bc-1.07.1.tar.gz -L https://ftp.gnu.org/gnu/bc/bc-1.07.1.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o binutils-2.28.tar.gz -L https://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o coreutils-8.27.tar.xz -L https://ftp.gnu.org/gnu/coreutils/coreutils-8.27.tar.xz
+curl --connect-timeout 10 -w 200 --retry 5 -o gmp-6.1.2.tar.bz2 -L https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2
+curl --connect-timeout 10 -w 200 --retry 5 -o mpfr-3.1.6.tar.bz2 -L https://ftp.gnu.org/gnu/mpfr/mpfr-4.0.1.tar.bz2
+curl --connect-timeout 10 -w 200 --retry 5 -o mpc-1.1.0.tar.gz -L https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o libpcap-1.9.0.tar.gz -L http://www.tcpdump.org/release/libpcap-1.9.0.tar.gz
+#curl --connect-timeout 10 -w 200 --retry 5 -o gcc-5.4.0.tar.gz -L https://ftp.gnu.org/gnu/gcc/gcc-5.4.0/gcc-5.4.0.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o gcc-8.2.0.tar.gz https://ftp.gnu.org/gnu/gcc/gcc-8.2.0/gcc-8.2.0.tar.gz
 curl --connect-timeout 10 -w 200 --retry 5 -o libffi-3.2.1.tar.xz -L https://github.com/libffi/libffi/archive/v3.2.1.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o autoconf-2.69.tar.gz -L https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
 curl --connect-timeout 10 -w 200 --retry 5 -o automake-1.16.1.tar.gz -L https://ftp.gnu.org/gnu/automake/automake-1.16.1.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o autoconf-2.69.tar.gz -L https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
 curl --connect-timeout 10 -w 200 --retry 5 -o libtool-2.4.tar.gz -L https://ftp.gnu.org/gnu/libtool/libtool-2.4.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o pkg-config-0.29.2.tar.gz -L https://pkg-config.freedesktop.org/releases/pkg-config-0.29.2.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o git-2.18.0.tar.gz -L https://github.com/git/git/archive/v2.18.0.tar.gz
-curl --connect-timeout 10 -w 200 --retry 5 -o expat-2.2.6.tar.gz -L https://github.com/libexpat/libexpat/archive/R_2_2_6.tar.gz
+curl --connect-timeout 10 -w 200 --retry 5 -o texinfo-6.5.tar.gz -L https://ftp.gnu.org/gnu/texinfo/texinfo-6.5.tar.gz
+
+#openssl
+curl --connect-timeout 10 -w 200 --retry 5 -o openssl-1.0.2q.tar.gz -L https://www.openssl.org/source/openssl-1.0.2q.tar.gz
+
+#python
+curl --connect-timeout 10 -w 200 --retry 5 -o Python-2.7.15.tgz -L https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz
+curl --connect-timeout 10 -w 200 --retry 5 -o virtualenv-16.1.0.tar.gz -L https://github.com/pypa/virtualenv/archive/16.1.0.tar.gz
+
+#scons
+curl --connect-timeout 10 -w 200 --retry 5 -o scons-3.0.1.tar.gz -L https://sourceforge.net/projects/scons/files/scons/3.0.1/scons-3.0.1.tar.gz/download
+
+
+
+
+
